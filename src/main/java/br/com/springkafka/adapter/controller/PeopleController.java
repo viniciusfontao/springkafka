94438b534d0a94c61d90df413c42c505c17d2405
Vinicius Almeida <vgfontao@gmail.com>
2022-01-03 20:24:03 -0300

    Refactor producer - clean arch

--- src/main/java/br/com/springkafka/adapter/controller/PeopleController.java
+++ src/main/java/br/com/springkafka/adapter/controller/PeopleController.java
@@ -1,8 +1,8 @@
 package br.com.springkafka.adapter.controller;
 
-import br.com.springkafka.PeopleAvro;
 import br.com.springkafka.adapter.dto.PeopleRequestDTO;
-import br.com.springkafka.adapter.producer.PeopleProducer;
+import br.com.springkafka.adapter.mapper.PeopleProducerDTOMapper;
+import br.com.springkafka.domain.usecase.PeopleProducerUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,28 +12,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/peoples")
 @AllArgsConstructor
 public class PeopleController {
 
-    private final PeopleProducer peopleProducer;
+    private final PeopleProducerUseCase peopleProducerUseCase;
+
+    private final PeopleProducerDTOMapper peopleProducerDTOMapper;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> sendMessage(@RequestBody PeopleRequestDTO peopleDTO) {
-        var id = UUID.randomUUID().toString();
+    public ResponseEntity<Void> sendMessage(@RequestBody PeopleRequestDTO peopleRequestDTO) {
 
-        var message = PeopleAvro.newBuilder()
-                .setId(id)
-                .setName(peopleDTO.getName())
-                .setCpf(peopleDTO.getCpf())
-                .setBooks(peopleDTO.getBooks().stream().map(p -> (CharSequence) p).collect(Collectors.toList()))
-                .build();
-
-        peopleProducer.sendMessage(message);
+        peopleProducerUseCase.execute(peopleProducerDTOMapper.toDTO(peopleRequestDTO));
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
